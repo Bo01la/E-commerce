dc446e7ddc92b00ca1858c58e8196acab28a49d0
Bo01la <abdoehab30@gmail.com>
2025-07-24 00:53:41 +0300

    url changes

--- client/src/Redux/Constants/BASE_URL.js
+++ client/src/Redux/Constants/BASE_URL.js
@@ -1,2 +1,3 @@
 // export const BASE_URL = "http://localhost:3000"
-export const BASE_URL = "https://e-commerce-chi-liard.vercel.app/";
+// export const BASE_URL = "https://e-commerce-chi-liard.vercel.app/";
+export const BASE_URL = "https://node-react-shop-pearl.vercel.app";
